52464b435a605f765447e645686b1ad30a9b7fe9
yuchen <dev.chenyu@gmail.com>
2022-01-12 15:22:52 +0800

    非课表页面添加提示

--- scheduleHtmlProvider.js
+++ scheduleHtmlProvider.js
@@ -1,8 +1,22 @@
-function scheduleHtmlProvider(iframeContent = "", frameContent = "", dom = document) {
-    //除函数名外都可编辑
-    //以下为示例，您可以完全重写或在此基础上更改
+async function scheduleHtmlProvider() {
 
-    // 抓取课表
-    let courseTable = document.querySelector("#courseTableBody");
-    return courseTable.outerHTML;
+    // 此步为必须，用于加载这个工具，后续会增加更多方法
+    await loadTool('AIScheduleTools')
+    const { AIScheduleAlert } = AIScheduleTools()
+
+    let courseTable;
+
+    try {
+        // 抓取课表
+        courseTable = document.querySelector("#courseTableBody").outerHTML;
+
+    } catch (e) {
+
+        console.log(e);
+        // 使用时务必带上await，否则没有系统alert的时停效果
+        await AIScheduleAlert('未发现课程信息，请在课表页面点开始导入')
+        return "do not continue";
+    }
+
+    return courseTable;
 }
